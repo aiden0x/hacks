23d852bed13fd6dc983b2fd8dd5c17c9e2bc96e7
alienus <mustafaahmedy04@gmail.com>
2025-11-03 00:27:41 +0200

    Improve wayback urls.

--- wayback/main.go
+++ wayback/main.go
@@ -1,135 +1,85 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
-	"net/url"
 	"os"
 	"strings"
+	"time"
 )
 
-const extensionsFilter = `original:.*\.(xls|xml|xlsx|json|pdf|sql|doc|docx|pptx|txt|zip|tar\.gz|tgz|bak|7z|rar|log|cache|secret|db|backup|yml|gz|git|config|csv|yaml|md|md5|exe|dll|bin|ini|bat|sh|tar|deb|rpm|iso|img|apk|msi|env|dmg|tmp|crt|pem|key|pub|asc)$`
+var extFilter = `xls|xml|xlsx|json|pdf|sql|doc|docx|pptx|txt|git|zip|tar\.gz|tgz|bak|7z|rar|log|cache|secret|db|backup|yml|gz|config|csv|yaml|md|md5|exe|dll|bin|ini|bat|sh|tar|deb|rpm|iso|img|env|apk|msi|dmg|tmp|crt|pem|key|pub|asc`
 
-func normalizeURL(raw string) (string, error) {
-	raw = strings.TrimSpace(raw)
-	if raw == "" {
-		return "", errors.New("empty URL")
+func buildURL(domain, fsc, ext string, inSubs, aext bool) string {
+	base := domain
+	if inSubs {
+		base = "*." + domain
 	}
-	if !strings.HasPrefix(raw, "http://") && !strings.HasPrefix(raw, "https://") {
-		raw = "https://" + raw
+	url := fmt.Sprintf("https://web.archive.org/cdx/search/cdx?url=%s/*&collapse=urlkey&output=text&fl=original,statuscode", base)
+	if fsc != "" {
+		url += fmt.Sprintf("&filter=statuscode:(%s)", fsc)
 	}
-	u, err := url.Parse(raw)
-	if err != nil {
-		return "", err
+	if aext {
+		url += fmt.Sprintf("&filter=original:.*.(%s)$", extFilter)
 	}
-	if u.Host == "" {
-		return "", errors.New("missing host")
+	if ext != "" {
+		url += fmt.Sprintf("&filter=original:.*.%s$", ext)
 	}
-	return u.String(), nil
+	return url
 }
 
-func buildWaybachURL(mode, hostname, fullPath string) (string, error) {
-	base := "https://web.archive.org/cdx/search/cdx"
-	values := url.Values{}
-
-	switch mode {
-	case "wildcard":
-		values.Set("url", fmt.Sprintf("*.%s/*", hostname))
-	case "domain":
-		values.Set("url", fmt.Sprintf("%s/*", hostname))
-	case "specific":
-		values.Set("url", fmt.Sprintf("%s/*", fullPath))
-	case "extensions":
-		values.Set("url", fmt.Sprintf("*.%s/*", hostname))
-		values.Set("filter", extensionsFilter)
-	default:
-		return "", fmt.Errorf("Unknown mode: %s", mode)
+func fetchCDX(url string) error {
+	client := &http.Client{
+		Timeout: time.Second * 30,
+		Transport: &http.Transport{
+			MaxIdleConns: 5,
+			MaxIdleConnsPerHost: 20,
+		},
 	}
 
-	values.Set("collapse", "urlkey")
-	values.Set("output", "text")
-	values.Set("fl", "original")
-
-	return fmt.Sprintf("%s?%s", base, values.Encode()), nil
-}
-
-func fetchCDX(urlStr, outPath string) error {
-	req, err := http.NewRequest(http.MethodGet, urlStr, nil)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return err
 	}
-	req.Header.Set("User-Agent", "wayback-cli/1.0")
+	req.Header.Set("User-Agent", "Mozilla/5.0")
 
-	res, err := http.DefaultClient.Do(req)
+	res, err := client.Do(req)
 	if err != nil {
 		return err
 	}
 	defer res.Body.Close()
-	if res.StatusCode < 200 || res.StatusCode > 299 {
-		return fmt.Errorf("Server returns: %v", err)
-	}
-
 	buf := &strings.Builder{}
 	if _, err = io.Copy(buf, res.Body); err != nil {
 		return err
 	}
-	str := buf.String()
-	fmt.Print(str)
-	if outPath != "" {
-		file, err := os.Create(outPath)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		if _, err := file.WriteString(str); err != nil {
-			return err
-		}
-		fmt.Printf("[+] Results Saved in: %s", outPath)
-	}
-	return err
+	fmt.Print(buf.String())
+	return nil
 }
 
 func main() {
-	urlStr := flag.String("u", "", "Target URL or domain")
-	mode := flag.String("m", "domain", "Mode: domain | wildcard | specific | extensions")
-	outFile := flag.String("o", "", "Save the results in to file")
-
+	domain := flag.String("d", "", "Target domain.")
+	inSubs := flag.Bool("s", false, "Include subdomains in searching URLs.")
+	fsc := flag.String("fsc", "", "Filter Status code you don't want to return it.")
+	ext := flag.String("ext", "", "Specific extentions.")
+	allext := flag.Bool("allext", false, "Match all extensions.")
 	flag.Parse()
 
-	if *urlStr == "" {
-		fmt.Println("[-] Error: --url is required")
+	if *domain == "" {
 		flag.Usage()
-		os.Exit(1)
+		fmt.Printf("\nsensitive file extensions [%s]\n\n", extFilter)
+		os.Exit(2)
 	}
 
-	normalizeURL, err := normalizeURL(*urlStr)
+	filter := *fsc
+	if strings.Contains(filter, ",") {
+		filter = strings.ReplaceAll(filter, ",", "|")
+	}
+	url := buildURL(*domain, filter, *ext, *inSubs, *allext)
+	//println(url)
+	err := fetchCDX(url)
 	if err != nil {
-		fmt.Println("[-] Invalid URL")
-		os.Exit(1)
+		fmt.Printf("[!] Error Fetching CDX: %s", err)
 	}
-
-	parsed, err := url.Parse(normalizeURL)
-	if err != nil {
-		fmt.Println("[-] Falied to parse URL")
-		os.Exit(1)
-	}
-
-	hostname := parsed.Hostname()
-	fullPath := parsed.Scheme + "://" + parsed.Host + parsed.Path
-
-	finalURL, err := buildWaybachURL(*mode, hostname, fullPath)
-	if err != nil {
-		fmt.Println("[-] Error:", err)
-		os.Exit(1)
-	}
-
-	fmt.Print("[+] Fetching Wayback URLs...\n")
-	if err := fetchCDX(finalURL, *outFile); err != nil {
-		fmt.Println("[-] Fetching error", err)
-		os.Exit(1)
-	}
-
 }
